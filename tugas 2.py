aa65f80902f030437098db6aa1624f48706e7a7c
iradestiana36-alt <iradestiana36@gmail.com>
2025-11-11 13:14:01 +0700

    Add files via upload

---
+++ tugas 2.py
@@ -0,0 +1,27 @@
+#python 3.7.1
+
+class AlatMusik:
+    nama1 = "Angklung"
+    jenis1 = "Getar"
+    bahan1 = "Bambu"
+    cara_main1 = "Digoyangkan"
+    asal1 = "Jawa Barat"
+    nama2 = "Gitar"
+    jenis2 = "Petik"
+    bahan2 = "Kayu & Senar"
+    cara_main2 = "Dipetik"
+    asal2 = "Jawa Barat"
+
+alatmusik1 = AlatMusik()
+alatmusik2 = AlatMusik()
+
+print(alatmusik1.nama1)
+print(alatmusik1.jenis1)
+print(alatmusik1.bahan1)
+print(alatmusik1.cara_main1)
+print(alatmusik1.asal1)
+print(alatmusik2.nama2)
+print(alatmusik2.jenis2)
+print(alatmusik2.bahan2)
+print(alatmusik2.cara_main2)
+print(alatmusik2.asal2)
